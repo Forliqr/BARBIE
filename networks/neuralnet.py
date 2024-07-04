b86113883616d754850a82b09701669bea5b55ad
Forliqr <168538434+Forliqr@users.noreply.github.com>
2024-07-04 08:40:16 +0800

    Add files via upload

---
+++ networks/neuralnet.py
@@ -0,0 +1,21 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size_list, num_classes):
+        super(NeuralNet, self).__init__()
+        self.dropout2 = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(input_size, hidden_size_list[0])
+        self.fc2 = nn.Linear(hidden_size_list[0], hidden_size_list[1])
+        self.fc3 = nn.Linear(hidden_size_list[1], num_classes)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = F.relu(out)
+        out = self.dropout2(out)
+        out = self.fc2(out)
+        out = F.relu(out)
+        out = self.fc3(out)
+        return out
